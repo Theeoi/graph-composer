984172fa6c56150cba47a7f08856543de59ddfe1
Theodor Blom <theodor.blom1@gmail.com>
2021-12-23 09:30:45 +0100

    Cleaned up project.

--- graph.py
+++ graph.py
@@ -1,65 +1,42 @@
-"""
-Implemented Markov Chain Composer Graph object by Kylie Ying
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import random
 
 class Vertex(object):
     def __init__(self, value):
-        self.value = value
-        self.adjacent = {}  # nodes that it points to
-        self.neighbors = []
-        self.neighbors_weights = []
-
-    def __str__(self):
-        return self.value + ' '.join([node.value for node in self.adjacent.keys()])
+        pass
 
     def add_edge_to(self, vertex, weight=0):
-        self.adjacent[vertex] = weight
+        pass
 
     def increment_edge(self, vertex):
-        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
+        pass
 
     def get_adjacent_nodes(self):
-        return self.adjacent.keys()
+        pass
 
     # initializes probability map
     def get_probability_map(self):
-        for (vertex, weight) in self.adjacent.items():
-            self.neighbors.append(vertex)
-            self.neighbors_weights.append(weight)
+        pass
 
     def next_word(self):
-        return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
+        pass
 
 
 
 class Graph(object):
     def __init__(self):
-        self.vertices = {}
+        pass
 
     def get_vertex_values(self):
-        return set(self.vertices.keys())
+        pass
 
     def add_vertex(self, value):
-        self.vertices[value] = Vertex(value)
+        pass
 
     def get_vertex(self, value):
-        if value not in self.vertices:
-            self.add_vertex(value)
-        return self.vertices[value]
+        pass
 
     def get_next_word(self, current_vertex):
-        return self.vertices[current_vertex.value].next_word()
+        pass
 
     def generate_probability_mappings(self):
-        for vertex in self.vertices.values():
-            vertex.get_probability_map()
+        pass
