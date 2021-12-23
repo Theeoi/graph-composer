9df802734fd1960636fdbcb3270e7953aad54b7d
Theodor Blom <theodor.blom1@gmail.com>
2021-12-23 11:00:35 +0100

    Implemented generator based on single text file.

--- graph.py
+++ graph.py
@@ -2,41 +2,48 @@ import random
 
 class Vertex(object):
     def __init__(self, value):
-        pass
+        self.value = value # The word from text input
+        self.adjacent = {}
+        self.neighbors = []
+        self.neighbor_weights = []
 
     def add_edge_to(self, vertex, weight=0):
-        pass
+        self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        pass
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1 # Get adjacent-number and increment
 
     def get_adjacent_nodes(self):
         pass
 
-    # initializes probability map
     def get_probability_map(self):
-        pass
+        for (vertex, weight) in self.adjacent.items():
+            self.neighbors.append(vertex)
+            self.neighbor_weights.append(weight)
 
     def next_word(self):
-        pass
+        return random.choices(self.neighbors, self.neighbor_weights)[0]
 
 
 
 class Graph(object):
     def __init__(self):
-        pass
+        self.vertices = {}
 
     def get_vertex_values(self):
-        pass
+        return set(self.vertices.keys())
 
     def add_vertex(self, value):
-        pass
+        self.vertices[value] = Vertex(value)
 
     def get_vertex(self, value):
-        pass
+        if value not in self.vertices: # Create new Vertex if word not in graph.
+            self.add_vertex(value)
+        return self.vertices[value] # Else return the object.
 
     def get_next_word(self, current_vertex):
-        pass
+        return self.vertices[current_vertex.value].next_word()
 
     def generate_probability_mappings(self):
-        pass
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
